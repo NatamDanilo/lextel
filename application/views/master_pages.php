91c13d83dee6ff57bdb97f5f2dd8d1953239bcd3
Eliasafe <li_okay@hotmail.com>
2017-10-13 12:13:48 -0300

    update

--- application/views/master_pages.php
+++ application/views/master_pages.php
@@ -1,9 +1,12 @@
 <?php 
 $this->load->view('includes/header');
 if($this->uri->segment(1) == 'residencial'):
-    $this->load->view('includes/menu_residencial');
+        // Carrega a view/menus residencial ou empresa, de acordo com get url.
+        $this->load->view('includes/menu_residencial');
+        $this->load->view('pages_residencial/'.$page);
     else:
-    $this->load->view('includes/menu_empresa');
+        $this->load->view('includes/menu_empresa');
+        $this->load->view('pages_empresa/'.$page);
 endif;
-$this->load->view('pages_residencial/'.$page);
+
 $this->load->view('includes/footer');
